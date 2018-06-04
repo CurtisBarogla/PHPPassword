382fa5307740dc4103550b0e5781d71d746d3b66
CurtisBarogla <curtis_barogla@outlook.fr>
2018-06-04 11:56:08 +0200

    Add possibility to generate passwords

---
+++ src/Generator/PasswordGeneratorInterface.php
@@ -0,0 +1,37 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Password component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace Ness\Component\Password\Generator;
+
+use Ness\Component\Password\Password;
+
+/**
+ * Generate secured password
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+interface PasswordGeneratorInterface
+{
+    
+    /**
+     * Generate a password of an artitrary length 
+     * 
+     * @param int $length
+     *   Password length
+     * 
+     * @return Password
+     *   Password generated
+     */
+    public function generate(int $length): Password;
+    
+}
